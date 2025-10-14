df683aec2f39b2d886272967ee4ae79149a33f66
niceWizzard <richard232manansala@gmail.com>
2025-10-14 13:51:52 +0800

    Removed pre-added packages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.serialization)
 
 }
 
@@ -59,8 +58,5 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
-    implementation(libs.koin.androidx.compose)
-    implementation(libs.koin.android)
-    implementation(libs.androidx.navigation.compose)
 
 }
